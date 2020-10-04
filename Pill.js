baed416a0cc0c6bf906a35454a98de7785125dc0
lightofpeace9891 <70032776+lightofpeace9891@users.noreply.github.com>
2020-10-04 18:07:32 -0400

    Update Pill.js

--- Pill.js
+++ Pill.js
@@ -1,5 +1,4 @@
-const pillShapes = ['Oblong', 'Round', 'Square', 'Oval', 'Rectangle', 'Diamond'];
-const pillColors = ['Red', 'Blue', 'Green', 'Yellow', 'Orange', 'Brown','Purple'];
+
 let Pill = function(name, shape, color) {
   this.name = name;
   this.shape = shape;
@@ -14,11 +13,11 @@ this.pillArr.push(new Pill("Adderall", "Oval", "Blue"));
 this.pillArr.push(new Pill("Advil PM", "Oval", "Blue"));
 
 function getPillsByShape(shape) {
-  console.log()
-  console.log('shape func ran')
+  
   var pillsByShape = [];
   for (i=0; i<this.pillArr.length; i++) {
     if (shape == this.pillArr[i].shape) {
+
       pillsByShape.push(this.pillArr[i]);
     }
   }
@@ -26,20 +25,13 @@ function getPillsByShape(shape) {
 }
 
 function getPillsByColor(color) {
-  console.log()
-  console.log('color func ran')
+  
   var pillsByColor = [];
   for (i=0; i<this.pillArr.length; i++) {
-    if ((color == this.pillArr[i].color) && (this.indexesAdded.has(i) == false)) {
+    if (color == this.pillArr[i].color) {
+      console.log('index: ' + i + ' color: ' + color);
       pillsByColor.push(this.pillArr[i]);
     }
   }
   return pillsByColor;
 }
-
-results = [];
-pillsShape = this.getPillsByShape('Oval'); 
-this.results.push(pillsShape); 
-pillsColor = this.getPillsByColor('Light Blue');
-this.results.push(pillsColor);
-console.log(results);
